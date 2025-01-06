749598d2afe80f874d6a61bd7510ed4136d93de6
Платонов Александр <platonovas@pochtabank.ru>
2025-01-06 16:07:41 +0400

    почти готовый вариант но без проверки CRUD

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
 
 type User struct {
-	ID        string
-	Firstname string
-	Lastname  string
-	Email     string
-	Age       uint
-	Created   time.Time
+	ID        string    `json:"id"`
+	Firstname string    `json:"firstname"`
+	Lastname  string    `json:"lastname"`
+	Email     string    `json:"email"`
+	Age       uint      `json:"age"`
+	Created   time.Time `json:"created"`
 }
 
 func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
@@ -61,6 +64,99 @@ func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getUserForBody(w http.ResponseWriter, r *http.Request) User {
+	// Читаем тело запроса
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Ошибка при чтении тела запроса", http.StatusInternalServerError)
+		return User{}
+	}
+	defer r.Body.Close() // Закрываем тело запроса после чтения
+
+	// Создаем экземпляр структуры User
+	var user User
+
+	// Преобразуем JSON в структуру
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, "Ошибка при преобразовании JSON", http.StatusBadRequest)
+		return User{}
+	}
+
+	fmt.Printf("Полученный пользователь: %+v\n", user)
+	return user
+}
+
+func (h *Handler) InsertUser(user User) {
+	// в целям упрощения примера пропущена валидация
+	result, err := h.DB.Exec(
+		"INSERT INTO users (id, firstname, lastname, email, age, created) VALUES ($1, $2, $3, $4, $5, $6)",
+		user.ID,
+		user.Firstname,
+		user.Lastname,
+		user.Email,
+		user.Age,
+		time.Now(),
+	)
+	__err_panic(err)
+
+	_, err = result.RowsAffected()
+	__err_panic(err)
+}
+
+func (h *Handler) Add(w http.ResponseWriter, r *http.Request) {
+
+	user := getUserForBody(w, r)
+
+	h.InsertUser(user)
+
+	http.Redirect(w, r, "/user", http.StatusFound)
+}
+
+func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
+	user := getUserForBody(w, r)
+
+	// в целям упрощения примера пропущена валидация
+	num, err := strconv.Atoi(user.ID)
+	__err_panic(err)
+	h.DeleteFromId(num)
+	h.InsertUser(user)
+
+	http.Redirect(w, r, "/user", http.StatusFound)
+}
+
+func (h *Handler) DeleteFromId(id int) {
+	fmt.Println("ID для удаления:", id)
+
+	result, err := h.DB.Exec(
+		"DELETE FROM users WHERE id = $1",
+		id,
+	)
+	__err_panic(err)
+
+	_, err = result.RowsAffected()
+	__err_panic(err)
+}
+
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+	// Извлекаем параметр id из строки запроса
+	query := r.URL.Query()
+	idStr := query.Get("id") // Получаем значение параметра "id"
+
+	// Преобразуем строку в целое число
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Неверный формат ID", http.StatusBadRequest)
+		return
+	}
+
+	__err_panic(err)
+
+	h.DeleteFromId(id)
+
+	http.Redirect(w, r, "/user", http.StatusFound)
+}
+
 type Handler struct {
 	DB   *sql.DB
 	Tmpl *template.Template
@@ -101,6 +197,9 @@ func main() {
 	// в целям упрощения примера пропущена авторизация и csrf
 	r := mux.NewRouter()
 	r.HandleFunc("/user", handlers.List).Methods("GET")
+	r.HandleFunc("/user/create", handlers.Add).Methods("POST")
+	r.HandleFunc("/user/update", handlers.Update).Methods("PATCH")
+	r.HandleFunc("/user/delete", handlers.Delete).Methods("DELETE")
 
 	fmt.Println("starting server at :8080")
 	http.ListenAndServe(":8080", r)
